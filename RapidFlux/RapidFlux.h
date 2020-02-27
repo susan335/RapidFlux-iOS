ea95273504147f908fab4cc060ef3474d61d809b
susan335 <susan.se3p@gmail.com>
2020-02-27 16:34:25 +0900

    Add framework header file

---
+++ RapidFlux/RapidFlux.h
@@ -0,0 +1,18 @@
+//
+//  RapidFlux.h
+//  RapidFlux
+//
+//  Created by Yohta Watanave on 2020/02/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RapidFlux.
+FOUNDATION_EXPORT double RapidFluxVersionNumber;
+
+//! Project version string for RapidFlux.
+FOUNDATION_EXPORT const unsigned char RapidFluxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RapidFlux/PublicHeader.h>
+
+
